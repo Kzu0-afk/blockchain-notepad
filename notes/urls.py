457f54300a4217d8d43e1e82136aefcb4dfc264c
gideongrid127@gmail.com <gideongrid127@gmail.com>
2025-11-24 14:36:46 +0800

    modified:   README.md 	modified:   db.sqlite3 	modified:   env.example 	modified:   notepad_project/settings.py 	modified:   notepad_project/urls.py 	modified:   notes/migrations/__pycache__/0001_initial.cpython-313.pyc 	modified:   notes/migrations/__pycache__/__init__.cpython-313.pyc 	modified:   notes/templates/notes/base.html 	modified:   notes/templates/notes/note_list.html 	modified:   notes/templates/notes/profile.html 	new file:   notes/templates/notes/send_ada.html 	modified:   notes/urls.py 	modified:   notes/views.py 	modified:   requirements.txt

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,5 @@
 # notes/urls.py
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 app_name = 'notes'
@@ -7,6 +7,7 @@ app_name = 'notes'
 urlpatterns = [
     # Landing page for non-authenticated users
     path('', views.landing_view, name='landing'),
+
     # Authentication URLs
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
@@ -18,4 +19,11 @@ urlpatterns = [
     path('notes/<int:pk>/delete/', views.note_delete_view, name='note_delete'),
     # Profile & Wallet URLs
     path('profile/', views.profile_view, name='profile'),
+
+    # WALLET & API ENDPOINTS
+    path('send-ada/', views.wallet_profile_view, name='send_ada'),
+    path('wallet/', views.wallet_profile_view, name='wallet_profile'),
+    path('api/build-transaction/', views.build_transaction, name='build_transaction'),
+    path('api/submit-transaction/', views.submit_transaction, name='submit_transaction'),
+    path('api/save-wallet/', views.save_wallet, name='save_wallet'),
 ]
